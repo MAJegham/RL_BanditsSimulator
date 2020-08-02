5b10e3680f8cf16ca8bec2c99fafec77a3c2d99e
Med Aziz Jegham <jeghammedaziz@gmail.com>
2020-08-02 22:10:35 +0200

    [rlsimulator] rlsimulator module base files

--- src/bandits/__init__.py
+++
@@ -1,7 +0,0 @@
-from .bandits import BernoulliBandit
-from .bandits import NormalBandit
-
-__all__ = [
-    "BernoulliBandit",
-    "NormalBandit"
-]
--- src/policies/__init__.py
+++
@@ -1,9 +0,0 @@
-from .policies import GreedyPolicy
-from .policies import EpsilonGreedyPolicy
-from .policies import UCBPolicy
-
-__all__ = [
-    "GreedyPolicy",
-    "EpsilonGreedyPolicy",
-    "UCBPolicy"
-]
